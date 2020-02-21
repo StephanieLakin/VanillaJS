570112b4a3c61f25c99b9fbdd056185394e5b64e
Stephanie Lakin <stephanielakin13@outlook.com>
2020-02-21 13:32:41 -0500

    form validation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
 // show input error meassage
 function showError(input, message) {
   const formControl = input.parentElement;
@@ -17,10 +18,13 @@ function showSuccess(input) {
   formControl.className = 'form-control success';
 }
 // Check email is valid
-
-function isValidEmail(email) {
+function checkEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
 // Check required fields
@@ -34,14 +38,28 @@ function checkRequired(inputArray) {
   });
 }
 
-checkLength(inut, min, max) {
-    if(imput.value.length < min) {
-        showError(input, `${getFieldName(inputk)} `)
-    }
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// checkPasswordsMatch
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match!');
+  }
 }
 // get fieldname
 
-function getFieldName(input) {
+function getFieldName(input) { 
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 // event listeners
@@ -50,32 +68,33 @@ form.addEventListener('submit', function(e) {
 
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
-  checkLength(password, 6, 26);
-  
-
-  //   if (username.value === '') {
-  //     showError(username, 'Username is required');
-  //   } else {
-  //     showSuccess(username);
-  //   }
-
-  //   if (email.value === '') {
-  //     showError(email, 'Email is required');
-  //   } else if (!isValidEmail(email.value)) {
-  //     showError(email, 'Email is not valid');
-  //   } else {
-  //     showSuccess(email);
-  //   }
-
-  //   if (password.value === '') {
-  //     showError(password, 'Password is required');
-  //   } else {
-  //     showSuccess(password);
-  //   }
-
-  //   if (password2.value === '') {
-  //     showError(password2, 'Confirm Password is required');
-  //   } else {
-  //     showSuccess(password2);
-  //   }
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
+
+//   if (username.value === '') {
+//     showError(username, 'Username is required');
+//   } else {
+//     showSuccess(username);
+//   }
+
+//   if (email.value === '') {
+//     showError(email, 'Email is required');
+//   } else if (!isValidEmail(email.value)) {
+//     showError(email, 'Email is not valid');
+//   } else {
+//     showSuccess(email);
+//   }
+
+//   if (password.value === '') {
+//     showError(password, 'Password is required');
+//   } else {
+//     showSuccess(password);
+//   }
+
+//   if (password2.value === '') {
+//     showError(password2, 'Confirm Password is required');
+//   } else {
+//     showSuccess(password2);
+//   }
